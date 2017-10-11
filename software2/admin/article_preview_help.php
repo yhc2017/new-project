e6df838603df2caf1853445921012d1e69d1950d
snailsmr <185466981@qq.com>
2017-10-11 21:54:08 +0800

    将后端整合到最新前端
    1.可直接复制张贴网页中的新闻
    2.新闻标题必须从文本文件中复制过去才能上传，直接复制网页新闻中的新闻标题将无法正常上传（虽然显示“上传成功”），无法写入数据库
    3.直接从网页中复制过来的新闻图片无法居中显示，可编辑性低
    4.部署前记得去更改数据库连接的用户名和自己的密码。在public/conn.php中更改

--- software2/admin/article_preview_help.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "../public/conn.php";
-$preContent = $_POST['precontent'];
-$prenewsTiele = "Temporary";
-$premyfile = fopen("../public/article/".iconv("UTF-8","GBK",$prenewsTiele).".txt", "w") or die("Unable to open file!");
-fwrite($premyfile, $preContent);
-fclose($premyfile);
-?>
